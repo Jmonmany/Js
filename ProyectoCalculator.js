e6d7b823e5d772a77168ced44b364dd27e36a068
Javi <monmany1992@gmail.com>
2022-08-16 12:21:58 +0200

    Calculator Basic Done

--- ProyectoCalculator.js
+++ ProyectoCalculator.js
@@ -1,27 +1,47 @@
 const Calculator = (a,b) => {
     let results = [];
     a = (prompt("Insert first value"))
-    b = (prompt("Insert second value"))
-    
-    if(isNaN(Number(a))||isNaN(Number(b))){
-        console.log("You can't do this operation!")
-    }else if(a===null&&b===null){
+    if(a===null){
         console.log("Please insert at least one number")
-    }
-    else if(a===null||b===null){
-        results.push(a,b)
-        let number = results.find(Number)
-        console.log("The square root of your number is " + Math.sqrt(number))
+        Calculator()
+    }else if(isNaN(Number(a))){
+        console.log("You can't do this operation!")
+        Calculator()
     }else{
-    a = Number(a)
-    b = Number(b)
-    let sum = Number((a+b).toFixed(3));
-    let subs = Number((a-b).toFixed(3));
-    let multi = Number((a*b).toFixed(3));
-    let div = Number((a/b).toFixed(3));
-    results.push(sum,subs,multi,div);
-    console.log(results)
-    console.log("The results are as follows; sum: " +results[0] +" subs:"+ results[1] +" multi: "+ results[2]+" division: "+results[3]);
+        let otherNumber = confirm("Would you like to insert numbers again?")
+        if(otherNumber === true){
+            const SecondNumber = () =>{
+                b = (prompt("Insert second value"))
+                if(b===null){
+                    console.log("Please insert at least one number")
+                    SecondNumber()
+                }else if(isNaN(Number(b))){
+                    console.log("You can't do this operation!")
+                    SecondNumber()
+                }
+                else if(Number(a) !== null && Number(b)!== null){
+                a = Number(a)
+                b = Number(b)
+                let sum = Number((a+b).toFixed(3));
+                let subs = Number((a-b).toFixed(3));
+                let multi = Number((a*b).toFixed(3));
+                let div = Number((a/b).toFixed(3));
+                results.push(sum,subs,multi,div);
+                console.log(results)
+                console.log("The results are as follows; sum: " +results[0] +" subs:"+ results[1] +" multi: "+ results[2]+" division: "+results[3]);
+                }
+            }
+            SecondNumber()
+        }else{
+            results.push(a)
+            console.log("The square root of your number is " + Math.sqrt(results[0]).toFixed(3))
+            let StartAgain = confirm("Would you like to insert numbers again?")
+            if(StartAgain === true){
+                Calculator()
+            }else{
+                console.log("Thanks you, see you next time")
+            }
+        }
     }
 }
 Calculator()
