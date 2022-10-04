8a8c791e0c4538d62afd544fc7b2feed7e343b27
Javi <monmany1992@gmail.com>
2022-10-04 16:22:57 +0200

    refactored

--- CalculatorPro.js
+++ CalculatorPro.js
@@ -1,48 +1,47 @@
-let Arrays = []
-const InsertNumbers = () =>{
+let mainArray = []
+
+const insertNumbers = () =>{
     do {
         number = prompt("Insert value")
-        if(number!==null){
-            Arrays.push(number);
-        }else if(number===null&&Arrays.length==0){
+        if(!number && mainArray.length == 0){
             alert("Please insert at least one number");
-            InsertNumbers();
+            insertNumbers();
         }
-        
-    } while (Arrays.length>0 && number !==null);
+        if(number){
+        mainArray.push(number);
+        }
+    } while (number);
 }
-InsertNumbers()
+insertNumbers()
 
-if(Arrays.length>1){
-    let ArrayOfNumbers = Arrays.map(element=>Number(element))
-    const sum = ArrayOfNumbers.reduce((sum, num) => sum + num);
-    const subs = ArrayOfNumbers.reduce((substraction, num) => substraction - num);
-    const multi = ArrayOfNumbers.reduce((multiplicacion, num) => multiplicacion * num);
-    const div = ArrayOfNumbers.reduce((division, num) => division / num);
-    console.log(`The results are as follows; SUM: ${sum.toFixed(3)} SUBSTRACTION: ${subs.toFixed(3)} MULTIPLICATION: ${multi.toFixed(3)} DIVISION: ${div.toFixed(3)}`);
-    
-}else if(ArrayOfNumbers.length==1){
-    console.log("The square root of your number is " + Number(Math.sqrt(Arrays[0]).toFixed(3)))
-}
-const Again = () =>{
-let YesOrNot = confirm("Do you want to set more numbers?")
-if(YesOrNot == true){
-        do {
-            number = (prompt("Insert value"))
-        if(number!==null){
-            Arrays.push(number);
-        }
-        } while (number !== null);
-    }else{
-        console.log("Thank you for your time, Bye!")
+const calculator = () => {
+    if(mainArray.length==1){
+        console.log("The square root of your number is " + Number(Math.sqrt(mainArray[0]).toFixed(3)))
+        return 
     }
-    if(Arrays.length>1){
-        let ArrayOfNumbers = Arrays.map(element=>Number(element))
-        const sum = ArrayOfNumbers.reduce((sum, num) => sum + num);
-        const subs = ArrayOfNumbers.reduce((substraction, num) => substraction - num);
-        const multi = ArrayOfNumbers.reduce((multiplicacion, num) => multiplicacion * num);
-        const div = ArrayOfNumbers.reduce((division, num) => division / num);
+    if(mainArray.length>1){
+        let arrayOfNumbers = mainArray.map(element=>Number(element))
+        
+        const sum = arrayOfNumbers.reduce((sum, num) => sum + num);
+        const subs = arrayOfNumbers.reduce((substraction, num) => substraction - num);
+        const multi = arrayOfNumbers.reduce((multiplicacion, num) => multiplicacion * num);
+        const div = arrayOfNumbers.reduce((division, num) => division / num);
+        
         console.log(`The results are as follows; SUM: ${sum.toFixed(3)} SUBSTRACTION: ${subs.toFixed(3)} MULTIPLICATION: ${multi.toFixed(3)} DIVISION: ${div.toFixed(3)}`);
-    }
+        return 
+    }  
 }
-Again()
+calculator()
+
+const startAgain = () => {
+
+    let answer = confirm("Do you want to set more numbers?")
+    if(answer){
+            insertNumbers()
+            calculator()
+    }else{
+            console.log("Thank you for your time, Bye!")
+    }
+       
+}
+startAgain()
